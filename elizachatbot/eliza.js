84e39062c34ea27dc181503eba6d20e3cd42fe70
swordslinger <58361978+swordslinger@users.noreply.github.com>
2024-12-19 13:46:52 +0000

    Still cant get Eliza giving correct responses tweaked patterns object format, going to work on respond function now

--- elizachatbot/eliza.js
+++ elizachatbot/eliza.js
@@ -12,83 +12,133 @@
    The use of 'const' + 'Object.freeze' ensures patterns wont be reassigned and the object itself cannot be modified. 
    */
 
-const patterns = Object.freeze({
-    
-    /* Regular expression to detect if user entered "hello, hi, hey"
-       Responses if match is detected */
-    [/(hello|hi|hey)/i]: [
-        "Hello! How are you feeling today?",
-        "Hi there! What’s on your mind?",
-        "Hey! How can I help you?"
+const patterns = {
+    greetingPatterns:[
+        {   
+            // Regular expression to detect if user entered "hello, hi, hey" 
+            regex:/\b(hello|hi|hey)(\s+.+)?/i,
+
+            // Responses if match is detected
+            responses: [
+                        "Hello! How are you feeling today?",
+                        "Hi there! What’s on your mind?",
+                        "Hey! How can I help you?"
+                        ],
+        }
+    ],
+    reminderPatterns:[
+        {
+            // Regular expression to detect if user entered "you remind of"
+            regex: /you\s*remind\s*me\s*of\s*(.+)/i,
+
+            // Responses if match is detected */
+            responses: [
+                        "Why do you think I remind you of {0}?",
+                        " What makes you think of {0} when talking to me?",
+                        "Is it a good feeling to be reminded of {0}?"
+
+                       ]
+        }
+    ],
+    familyPatterns:[
+        {
+            // Regular expression to detect if user entered "mother, father, family, parent"
+            regex: /you\s*remind\s*me\s*of\s*(.+)/i,
+
+            // Responses if match is detected.
+            responses:[
+                "Tell me more about your family.",
+                "How does that make you feel about your family?",
+                "What role does your family play in your thoughts?"
+            ]
+        }
     ],
 
-    /* Regular expression to detect if user entered "you remind of"
-       Responses if match is detected */
-    [/(you remind me of)(.*)/i]: [
-        "Why do you think I remind you of {0}?",
-        "What makes you think of {0} when talking to me?",
-        "Is it a good feeling to be reminded of {0}?"
+    needPatterns:[
+        {
+            // Regular expression to detect if user entered "I need x"
+            regex:/i\s*need\s+to\s+(.+)/i,
+            
+            // Responses parsing x if match is detected.
+            responses:[
+                "Why do you need {1}?",
+                "Would getting {1} really help you?",
+                "What if you didn’t need {1}?"
+            ]
+        }
     ],
 
-    /* Regular expression to detect if user entered "mother, father, family, parent"
-       Responses if match is detected */
-    [/(.*)(mother|father|family|parent)(.*)/i]: [
-        "Tell me more about your family.",
-        "How does that make you feel about your family?",
-        "What role does your family play in your thoughts?"
+    feelingPatterns: [
+        {
+            // Regular expression to detect if user entered "I am x".
+            regex: /i\s*am\s+(feeling\s+)?(.+)/i,
+
+            //Responses parsing x if match is detected.
+            responses:[
+                "Why do you think you are {1}?",
+                "How long have you felt that way?",
+                "What made you feel like {1}?"
+            ],
+        },
+
+        {
+            // Regular expression to detect if user entered "I feel x"
+            regex: /i\s*feel\s+(.+)/i,
+
+            // Responses parsing x if match is detected. */
+            responses: [
+                "Why do you feel {1}?",
+                "Does feeling {1} happen often?",
+                "How does that feeling affect you?"
+            ],
+        }
     ],
 
-    /* Regular expression to detect if user entered "I need x"
-       Responses parsing x if match is detected. */
-    [/(.*) I need (.*)/i]: [
-        "Why do you need {1}?",
-        "Would getting {1} really help you?",
-        "What if you didn’t need {1}?"
+    sorryPatterns: [
+        {
+             // Regular expression to detect if user entered "sorry/apologize" 
+            regex: /\b(sorry|apologize)\b/i,
+
+            // Responses if match is detected.
+            responses: [
+                "No need to apologize.",
+                "Apologies aren't necessary. Why do you feel that way?",
+                "It’s okay to feel that way."
+            ]
+        }
     ],
 
-    /* Regular expression to detect if user entered "I am x"
-       Responses parsing x if match is detected. */
-    [/(.*) I am (.*)/i]: [
-        "Why do you think you are {1}?",
-        "How long have you felt that way?",
-        "What made you feel like {1}?"
+    goodbyePatterns: [
+        {
+            // Regular expression to detect if user entered "bye, goodbye, exit"
+            regex: /\b(sorry|apologize)\b/i,
+
+            //  Responses if match is detected. 
+            responses: [
+                "Goodbye! Take care.",
+                "Thank you for sharing. Goodbye!",
+                "Bye! I’m here if you need to talk again "
+            ]
+        }
     ],
 
-    /* Regular expression to detect if user entered "I feel x"
-       Responses parsing x if match is detected. */
-    [/(.*) I feel (.*)/i]: [
-        "Why do you feel {1}?",
-        "Does feeling {1} happen often?",
-        "How does that feeling affect you?"
-    ],
+    generalPatterns: [
+        {
+            // Regular expression catch all if user enters invalid prompt.
+            regex: /.+/i,
 
-    /* Regular expression to detect if user entered "sorry/apologize"
-       Responses if match is detected. */
-    [/(.*) (sorry|apologize) (.*)/i]: [
-        "No need to apologize.",
-        "Apologies aren't necessary. Why do you feel that way?",
-        "It’s okay to feel that way."
-    ],
-
-    /* Regular expression to detect if user entered "bye, goodbye, exit"
-       Responses if match is detected. */
-    [/(.*) (bye|goodbye|exit) (.*)/i]: [
-        "Goodbye! Take care.",
-        "Thank you for sharing. Goodbye!",
-        "Bye! I’m here if you need to talk again."
-    ],
-
-    /* Regular expression catch all if user enters invalid prompt"
-       Responses if match is detected. */
-    [/(.*)/i]:[
-        "Can you tell me more?",
-        "Why do you say that?",
-        "How does that make you feel?",
-        "What do you mean by that?",
-        "Interesting... go on."
+            // Responses if match is detected.
+            responses: [
+                "Can you tell me more?",
+                "Why do you say that?",
+                "How does that make you feel?",
+                "What do you mean by that?",
+                "Interesting... go on."
+            ]
+        }
     ]
 
-});
+}
 
 // reflections object to mirror user input.
 const reflections = Object.freeze({
@@ -117,10 +167,10 @@ function reflect(text){
 function respond(user_input){
     
     // Loop through the patterns object and store the regular expression in the pattern and the responses in the responses list.
-    for (const [pattern, responsesList] of Object.entries(patterns)){
+    for(const [pattern, responses] of Object.entries(patterns)){
         
         // regex object to store a case insensitive version of the current regular expression.
-        const regex = new RegExp(pattern,'i')
+        const regex = new RegExp(pattern[0], "i")
         
         // run the regular expression on the user's input.
         const match = regex.exec(user_input)
@@ -130,7 +180,8 @@ function respond(user_input){
 
             // https://stackoverflow.com/questions/4550505/getting-a-random-value-from-a-javascript-array
             // select a pseudo-random response from responses list.
-            const response = responsesList[Math.floor(Math.random() * responsesList.length)]
+            
+            const response = responses[Math.floor(Math.random() * responses.length)]
 
             // https://www.decodingweb.dev/javascript-list-comprehension
             // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
@@ -139,16 +190,17 @@ function respond(user_input){
 
             // if the reflected groups contains any strings inset them into response.
             if(reflected_groups.length > 0){
-                
                 // Replace the index in the response with the reflected group.
-                return reflected_groups.reduce((response, group, index) =>
-                    response.replace(`{${index}}`, group), response)
-            
-            // If there are no reflected groups return the response as is.
+                return reflected_groups.reduce((response, group, index) => {
+                        return response.replace(`{${index + 1}}`, group)
+                }, response)
             } else {
                 return response
             }
-        } // EO if statement
+            
+            // If there are no reflected groups return the response as is.
+             // EO if statement
+        }
 
     } // EO for loop.
 
